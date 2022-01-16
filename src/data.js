edcc822d88c6ea0530b0e29e6de14c47e26ed120
EddieNjoroge <njorogeeddie434@gmail.com>
2022-01-16 04:23:10 +0300

    A few modifications to display data

---
+++ src/data.js
@@ -0,0 +1,63 @@
+const foreword = `Learning has always been a major part of my life. \
+It is one of those things that just awakens the best in me, one of the \
+things that I really enjoy. Applying theknowledge I have gained is among \
+the things that give me pleasure and encourage me to wake up every morning. \
+Close to that are the opportunities to apply this knowledge and the ability to \
+learn something new.`
+
+const finalWord = `In conclusion, I like a challenge. Any environment designed \
+to get me out of my comfort zone is a welcome. \
+I like growth, evolution and opportunities for that happening in my, and other \
+people's lives. I love being a witness to change for the better. \
+I would prefer to live in a world where innovation is made easy and not bombarded \
+by unnecessary bureaucracies. One where there is always room to move forward in all \
+aspects of life.`
+
+const schools = {
+    "University": {
+        "name":"Kenyatta University",
+        "course":"Mechanical Engineering",
+        "start_year":2018,
+        "end_year":2025
+    },
+    "highschool":{
+        "name":"Maseno School",
+        "start_year":2014,
+        "end_year":2017
+    },
+    "primary":{
+        "name":"Gilgil DEB Primary School",
+        "start_year":2006,
+        "end_year":2014
+    }
+}
+
+const certs = {
+    "freecodecamp.org":{
+        "Responsive Web Design": "https://www.freecodecamp.org/certification/eddie-w-njoroge/responsive-web-design",
+        "Javascript Algorithms And Data Structures": "https://www.freecodecamp.org/certification/eddie-w-njoroge/javascript-algorithms-and-data-structures",
+        "Front End Development Libraries": "",
+        "Data Visualization": "",
+        "Backend Development And APIs": "",
+        "Quality Assurance": "",
+        "Scientific Computing With Python": "",
+        "Data Analysis With Python": "",
+        "Information Security": ""
+    },
+    "udemy.com":{
+        "The Compelete Ethical Hacking Course!":"",
+        "Machine Learning & Deep Learning in Python & R": "",
+        "Advanced Neural Networks in R - A Practical Approach": "",
+        "Artificial Intelligence in Web Design": "",
+        "Ultimate Ethical Hacking and Penetration Testing": ""
+    },
+    "aws certifications":{
+
+    },
+
+    "google certificates":{
+
+    }
+}
+const data = { foreword, finalWord, schools, certs }
+export default data
